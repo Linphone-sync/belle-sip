4a126269221aa3d68a13a356d3dc6a6250fa0865
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-02-15 13:12:07 +0100

    Visual Studio project working on Windows Phone 8.

---
+++ build/windows/cain-sip-tester-wp8/cain-sip-tester-native/cain-sip-tester-native.cpp
@@ -0,0 +1,15 @@
+﻿// cain-sip-tester-native.cpp
+#include "pch.h"
+#include "cain-sip-tester-native.h"
+
+using namespace cain_sip_tester_native;
+using namespace Platform;
+
+CainSipTesterNative::CainSipTesterNative()
+{
+}
+
+void CainSipTesterNative::run()
+{
+	cain_sip_tester_run_tests("Resolver", 0);
+}
---
+++ build/windows/cain-sip-tester-wp8/cain-sip-tester-native/cain-sip-tester-native.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+extern "C" {
+	int cain_sip_tester_run_tests(char *suite_name, char *test_name);
+}
+
+namespace cain_sip_tester_native
+{
+    public ref class CainSipTesterNative sealed
+    {
+    public:
+        CainSipTesterNative();
+		void run();
+    };
+}
